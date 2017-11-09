3064f8b24dc186046f2e89ab10a6818fb71d23cb
scotthadzik <smhadzik@parimote.com>
2017-11-08 19:54:46 -0700

    installed bower package angular-content-editable

---
+++ MyYouthFutures/wwwroot/lib/angular-content-editable/src/angular-content-editable.provider.js
@@ -0,0 +1,26 @@
+angular.module('angular-content-editable')
+
+/**
+ * Provider to setup the default
+ * module options for the directive
+ */
+.provider('contentEditable', function () {
+
+  var defaults = {
+    editableClass: 'editable',
+    keyBindings: true, // default true for key shortcuts
+    singleLine: false,
+    focusSelect: true, // default on focus select all text inside
+    renderHtml: false,
+    editCallback: false
+  }
+
+  this.configure = function (options) {
+    return angular.extend(defaults, options);
+  }
+
+  this.$get = function () {
+    return defaults;
+  }
+
+});
