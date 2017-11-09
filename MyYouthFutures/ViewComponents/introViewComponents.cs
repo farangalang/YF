d70cfd9518ab5e4b87579e87a9073afb025932e7
TheSteve034 <swreeder88@gmail.com>
2017-11-09 10:54:26 -0700

    added the inrotViewComponent class

---
+++ MyYouthFutures/ViewComponents/introViewComponents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+//this class holds the call for the intro article on the about page.
+namespace MyYouthFutures.ViewComponents
+{
+    public class introViewComponents : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
