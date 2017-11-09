674ce203142bdebc9048bc00fad4c307df0dc7af
scotthadzik <smhadzik@parimote.com>
2017-11-08 22:37:50 -0700

    Functioning edit without dbsave. Model dynamicaly populated

--- MyYouthFutures/wwwroot/js/editController.js
+++ MyYouthFutures/wwwroot/js/editController.js
@@ -1,21 +1,29 @@
 ﻿(function () {
     'use strict';
 
-    angular
-        .module('app')})
-        .controller('editController', editController, editFunction());
+    angular.module('app')
+        .controller('editController',
+        function ($scope) {
 
-    editController.$inject = ['$location'];
+            $scope.showSave = false;
 
-    function editController($location) {
-        /* jshint validthis:true */
-        var vm = this;
-        vm.title = 'editController';
-        vm.showSave = false;
+                $scope.editMade = function(text, elem) {
+                    alert("EDIT: text = " + text + " ELEMENT = :" + elem);
+                }
+
+            });
+
+    //editController.$inject = ['$location'];
+
+    //function editController($location) {
+    //    /* jshint validthis:true */
+    //    var vm = this;
+    //    vm.title = 'editController';
+    //    vm.showSave = false;
 
 
-    }
-    function editFunction(text, elem) {
-        alert("edit" + text + " : " + elem);
-    }
+    //}
+    //function editFunction(text, elem) {
+    //    alert("edit" + text + " : " + elem);
+    //}
 })();
