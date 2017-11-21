9cc52c02d209480465ff01a672f23598a01311f0
scotthadzik <smhadzik@parimote.com>
2017-11-20 21:06:00 -0700

    Added Add new Service and two edit helpers

--- MyYouthFutures/Models/Services_Message.cs
+++ MyYouthFutures/Models/Services_Message.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,14 @@ namespace MyYouthFutures.Models
     public class Services_Message
     {
         public int ID { get; set; }
+        [Display(Name = "Image Location")]
         public string MessageImage { get; set; }
+        [Display(Name = "Service Header")]
         public string MessageHeader { get; set; }
+        [Display(Name = "Service Description")]
+        [DataType(DataType.MultilineText)]
         public string Message { get; set; }
+        [Display(Name = "Link to Page")]
         public string Link { get; set; }
     }
 }
