a0877ede9669f6f1975ca0a6ce950cd5ae8f216f
RBerghout <rachelberghout@mail.weber.edu>
2017-11-25 12:16:23 -0700

    Added model for Youth Stories

---
+++ MyYouthFutures/Models/Youth_Stories.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyYouthFutures.Models
+{
+    public class Youth_Stories
+    {
+        public int id { get; set; }
+        public DateTime date { get; set; }
+        public string title { get; set; }
+        public string image { get; set; }
+        public string imageBlurb { get; set; }
+        public string articleText { get; set; }
+    }
+}
