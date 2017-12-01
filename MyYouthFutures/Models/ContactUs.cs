6ed3ce9936e2128ae9bae7407011ee4fc42c6780
Jeffrey Garcia <jeffreygarcia3843@gmail.com>
2017-12-01 00:32:58 -0700

    Added ContactUs class for the Contact Us page.

---
+++ MyYouthFutures/Models/ContactUs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyYouthFutures.Models
+{
+    public class ContactUs
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Message { get; set; }
+        public string Telephone { get; set; }
+    }
+}
