2853ebc5c5d199e2591cbb828aff26f5ca80fbf7
scotthadzik <smhadzik@parimote.com>
2017-12-01 16:56:31 -0700

    Removed views to models that have been deleted

--- MyYouthFutures/Data/YouthContext.cs
+++ MyYouthFutures/Data/YouthContext.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using MyYouthFutures.Models;
 using Microsoft.EntityFrameworkCore;
-using MyYouthFutures.Models.Entities;
 
 namespace MyYouthFutures.Data
 {
-    public class YouthContext : IdentityDbContext<StoreUser>
+    public class YouthContext : DbContext
     {
         public YouthContext(DbContextOptions<YouthContext> options) : base(options)
         {
@@ -17,7 +15,8 @@ namespace MyYouthFutures.Data
 
         public DbSet<Link> Links { get; set; }
         public DbSet<Purpose> Purposes { get; set; }
-        public DbSet<Services_Message> Services_Messages { get; set; }   
+        public DbSet<Services_Message> Services_Messages { get; set; }
+        
         public DbSet<introArticle> introArticles { get; set; } //this is the table that holds the introduction statment on the history page.
         public DbSet<Founder_Message> Founder_Message { get; set; }//this the table that holds the founder message panels
         public DbSet<FirstYear_Service_Messages> FirstYear_Service_Messages { get; set; } //this is the table that holds the first year service message panels
