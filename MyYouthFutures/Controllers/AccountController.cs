e264edabe485840969421c8589fb1c2bb8f0eafc
scotthadzik <smhadzik@parimote.com>
2017-12-01 20:01:16 -0700

    Login non-functioning

--- MyYouthFutures/Controllers/AccountController.cs
+++ MyYouthFutures/Controllers/AccountController.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyYouthFutures.Models.Entities;
+using MyYouthFutures.ViewModel;
 
 namespace MyYouthFutures.Controllers
 {
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<StoreUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger,
+            SignInManager<StoreUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
         public IActionResult Login()
@@ -26,5 +32,35 @@ namespace MyYouthFutures.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(
+                    model.UserName,
+                    model.Password,
+                    false,
+                    false);
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        RedirectToAction("Index", "Home");
+                    }
+                    
+                }
+
+            }
+
+            ModelState.AddModelError("","Failed to Login");
+            
+
+            return View();
+        }
     }
 }
--- MyYouthFutures/Data/YouthContext.cs
+++ MyYouthFutures/Data/YouthContext.cs
@@ -17,8 +17,7 @@ namespace MyYouthFutures.Data
 
         public DbSet<Link> Links { get; set; }
         public DbSet<Purpose> Purposes { get; set; }
-        public DbSet<Services_Message> Services_Messages { get; set; }
-        
+        public DbSet<Services_Message> Services_Messages { get; set; }    
         public DbSet<introArticle> introArticles { get; set; } //this is the table that holds the introduction statment on the history page.
         public DbSet<Founder_Message> Founder_Message { get; set; }//this the table that holds the founder message panels
         public DbSet<FirstYear_Service_Messages> FirstYear_Service_Messages { get; set; } //this is the table that holds the first year service message panels
