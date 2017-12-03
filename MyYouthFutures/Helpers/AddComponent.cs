2bbd9670351b0a229fb0720ef2fa6711a46dfb76
scotthadzik <smhadzik@parimote.com>
2017-12-02 17:40:54 -0700

    Authentication checking completed for services modified tag helpers

--- MyYouthFutures/Helpers/AddComponent.cs
+++ MyYouthFutures/Helpers/AddComponent.cs
@@ -1,33 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace MyYouthFutures.Helpers
 {
-    public class EditLink : TagHelper
+    public class AddComponent: TagHelper
     {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Value { get; set; }
+        public string href { get; set; }
         public bool IsVisible { get; set; } = true;
 
         public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             if (!IsVisible)
             {
-                output.TagName = "div";
-                output.TagMode = TagMode.StartTagAndEndTag;
-                output.Attributes.SetAttribute("is-visible", "User.Identity.IsAuthenticated");
+                output.SuppressOutput();
             }
             else
             {
-                output.TagName = "div";
+                href = "/" + Controller + "/" + Action;
+                output.TagName = ("a");
                 output.TagMode = TagMode.StartTagAndEndTag;
-                output.Attributes.SetAttribute("class", "middleContainer");
-                output.Attributes.SetAttribute("is-visible", "User.Identity.IsAuthenticated");
+                output.Attributes.SetAttribute("class", "editText");
+                output.Attributes.SetAttribute("href", href);
             }
             return base.ProcessAsync(context, output);
         }
-    }
 
         
     }
+}
--- MyYouthFutures/Helpers/EditA.cs
+++ MyYouthFutures/Helpers/EditA.cs
@@ -13,16 +13,24 @@ namespace MyYouthFutures.Helpers
         public string Controller { get; set; }
         public string RouteID { get; set; }
         public string href { get; set; }
+        public bool IsVisible { get; set; } = true;
 
-
-        public override void Process(TagHelperContext context, TagHelperOutput output)
-        { 
-            href = "/" + Controller + "/Edit/" +RouteID;
-            output.TagName = ("a");
-            output.TagMode = TagMode.StartTagAndEndTag;
-            output.Attributes.SetAttribute("class", "editText");
-            output.Attributes.SetAttribute("href", href);
-            output.Content.SetContent("Edit");
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!IsVisible)
+            {
+                output.SuppressOutput();
+            }
+            else
+            {
+                href = "/" + Controller + "/Edit/" + RouteID;
+                output.TagName = ("a");
+                output.TagMode = TagMode.StartTagAndEndTag;
+                output.Attributes.SetAttribute("class", "editText");
+                output.Attributes.SetAttribute("href", href);
+                output.Content.SetContent("Edit");
+            }
+            return base.ProcessAsync(context, output);
         }
     }
 }
