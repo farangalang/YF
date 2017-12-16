ac2fd49b4d7797b27fab34fe89c550e3ffc6e095
TheSteve034 <swreeder88@gmail.com>
2017-12-15 22:37:13 -0700

    added a new attribute to the list items model. It now has a group ID. This can be grabed by the edit logic so that we only grab items that are logically linked together. We need some logic in the create new list items that checks for a free group number when adding in items that are part of a new group. we also need to the add item logic to and the group number when creating a new list item in a group that is already in place. I have also set the DB initalizer to populate the new group IDs

--- MyYouthFutures/Models/Entities/List_Item.cs
+++ MyYouthFutures/Models/Entities/List_Item.cs
@@ -9,6 +9,10 @@ namespace MyYouthFutures.Models
     {
         public int ID { get; set; }
         public string TypeOfList { get; set; }
+        /// <summary>
+        /// the group ID property gives a digit for the UI to grab when getting the edit view. This way list items can be edited as logical groups
+        /// </summary>
+        public int GroupID { get; set; }
         public string LiText { get; set; }
     }
 }
